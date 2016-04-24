33d658e492c58ca209e7e8d67a9b06aadec83730
N1ck0 G0m3z <christiangomez1992@gmail.com>
2016-04-24 16:46:13 +0900

    Update 20160423.rb

--- submit/ゴメス・クリスアン/20160423.rb
+++ submit/ゴメス・クリスアン/20160423.rb
@@ -2,19 +2,22 @@
     puts "Hello World"
 end
 
-(01...10).each do |n|
+(1...10).each do |n|
     (01...10).each do |m|
-        print n*m, " "
+        res = n*m
+        print '%.2i '%res
     end
     print "\n"
 end
 
-(01...100).each do |n|
+(1..100).each do |n|
     if n%3==0 and n%5==0
         puts "Fizz Buzz"
     elsif n%3==0
         puts "Fizz"
     elsif n%5==0
         puts "Buzz"
+    else
+        puts n
     end
 end
