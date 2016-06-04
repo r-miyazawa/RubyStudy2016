e1e57a9c386c25bd9b581bc5cf103f1eecf5d4bf
idekousuke <com-support@compotal.jp>
2016-06-04 14:41:38 +0900

    commit

---
+++ submit/idekousuke/Q7/toranp.rb
@@ -0,0 +1,23 @@
+card = []
+
+mk = ["Spade", "Heart", "Dia", "Club"]
+
+num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q" ,"K"]
+
+
+
+
+
+for i in(0..3)
+
+  for k in(0..12)
+
+    card << mk[i] + num[k].to_s
+
+  end
+
+end
+
+
+p num.sample(1)
+
