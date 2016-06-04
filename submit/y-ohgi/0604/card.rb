aa379363fa5ece36b174a6b7852b690b66368b4e
y-ohgi <ohgi.211@gmail.com>
2016-06-04 15:27:27 +0900

    add Q7ans

---
+++ submit/y-ohgi/0604/card.rb
@@ -0,0 +1,50 @@
+# coding: utf-8
+
+# １枚のカードを表すクラス
+class Card
+  # スートの記号
+  @@suit_mark = ['$', '♢', '♡', '♠', '♣']
+  # スートの名称
+  @@suit_name = ['ジョーカー', 'ダイヤ', 'ハート', 'スペード', 'クラブ']
+  
+  @number # カードのナンバー
+  @suit # カードのスート
+
+  def initialize(num, suit)
+    # 例外処理
+    if !num.kind_of?(Integer) || !suit.kind_of?(Integer)
+      return
+    elsif num < 1 || num > 13
+      return
+    elsif suit < 0 || suit > 4
+      return
+    end
+
+    @num = num
+    @suit = suit
+  end
+
+  # カード情報を取得
+  def get_detail
+    return [get_number, get_suit]
+  end
+
+  # カードのナンバーを取得
+  def get_number
+    return @num
+  end
+
+  # カードのスートを取得
+  def get_suit(type = nil)
+    if type == "mark"
+      return @@suit_mark[@suit]
+    elsif type == "name"
+      return @@suit_name[@suit]
+    else
+      return @suit
+    end
+  end
+  
+end
+
+card = Card.new(13, 2)
