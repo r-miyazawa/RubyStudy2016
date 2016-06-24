dba00eb79f587e85f6f798b00540345189f1ddc5
y-ohgi <ohgi.211@gmail.com>
2016-06-24 17:45:23 +0900

    add q1 tests

---
+++ submit/y-ohgi/0618/q1.rb
@@ -0,0 +1,41 @@
+# coding: utf-8
+# 問1.
+# 1.1〜100までの整数が昇順に並ぶ配列aを作成せよ
+# 2.[1]の配列の各要素を100倍した、新しい配列bを作成せよ
+# 3.[1]の配列の各要素から3の倍数のみ取得した、新しい配列cを作成せよ
+# 4.[1]の配列を逆順に並び替えせよ
+# 5.[1]の配列に含まれる整数の和を求めよ
+# 6.数値からなる配列array1と配列array2のそれらのここの要素を足し合わせた結果を配列で返すsum_arrayメソッドを作成せよ
+# 実行例）
+# p sum_array([1,2,3],[4,5,6]) # 出力: [5,7,9]
+
+
+# startnum..endnumまでの配列を作成し、その配列を返す
+def makeNumAry(startnum, endnum)
+  return []
+end
+
+# 渡された数値が格納された配列を全て100倍し、その配列を返す
+def doHundredfoldNumAry(numary)
+  return []
+end
+
+# 渡された数値が格納された配列を全て3の倍数を抽出し、その配列を返す
+def getTrebleNumAry(numary)
+  return []
+end
+
+# 渡された数値が格納された配列を逆順にし、その配列を返す
+def reverseNumAry(numary)
+  return []
+end
+
+# 渡された数値が格納された配列の要素全ての和を求め、その数値を返す
+def addNumAry(numary)
+  return 0
+end
+
+# 渡された2つの、数値が格納された配列を個々に足しあわせ、その配列を返す
+def sum_array(ary0, ary1)
+  return []
+end
