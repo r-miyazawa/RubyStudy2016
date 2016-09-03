8c944be58fae128b6fe6ad52fdd02f9b34802d63
idekousuke <com-support@compotal.jp>
2016-09-03 15:33:31 +0900

    できました

--- submit/idekousuke/Q7/toranp.rb
+++ submit/idekousuke/Q7/toranp.rb
@@ -1,23 +1,21 @@
-card = []
-
+class Card
+mk=0
+num=0
+ca = Array.new()
 mk = ["Spade", "Heart", "Dia", "Club"]
-
+ 
 num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q" ,"K"]
-
-
-
-
-
+ 
 for i in(0..3)
 
-  for k in(0..12)
-
-    card << mk[i] + num[k].to_s
-
-  end
+for k in(0..12)
+ca << mk[i] + num[k].to_s
+end
 
 end
 
+p ca[rand(0..51)] 
+end
 
-p num.sample(1)
+card=Card.new()
 
