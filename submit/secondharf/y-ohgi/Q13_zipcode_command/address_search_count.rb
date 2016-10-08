ea255ee3a48e9e9f034c9bdeb2e74d07ac4ca6c1
y-ohgi <ohgi.211@gmail.com>
2016-10-08 14:55:25 +0900

    add Q13

---
+++ submit/secondharf/y-ohgi/Q13_zipcode_command/address_search_count.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'sqlite3'
+
+require './config.rb'
+
+class Search
+
+  def initialize(term)
+    raise "DBが無い" unless File.exists?(DB_NAME)
+    @term = term
+  end
+
+  def execute
+    sql = "SELECT * FROM zips WHERE alladdr LIKE ?"
+    ary = []
+
+    SQLite3::Database.open(DB_NAME) do |db|
+      db.execute(sql, "%#{@term}%") do |row|
+        ary << sprintf("%s %s", row[0], row[4]) << "\n"
+      end
+    end
+    puts "結果:[count=#{ary.count}"
+  end
+  
+end
+
+Search.new(ARGV[0]).execute
