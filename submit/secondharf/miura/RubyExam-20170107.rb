50c01d3a1028f5285c7a642e65445b1f3511a630
miura <baske_uver.like@me.com>
2017-01-19 20:52:40 +0900

    1/7再up

--- submit/secondharf/miura/RubyExam-20170107.rb
+++ submit/secondharf/miura/RubyExam-20170107.rb
@@ -1,43 +1,33 @@
 =begin
 復習問題
-
 1.以下の出力となるコードを記述せよ
-
 1)
 *             
 **       
 ***           
 ****    
 *****     
-
 2-1)
     *    
    ***     
   *****   
  ******* 
 *********      
-
 2-2)               
 ********* 
  *******      
   *****       
    ***    
     *
-
 2.do-whileを使用して以下の出力となるコードを記述せよ
-
 50 49 47 44 40 35 29 22 14 5 -5
-
 3.以下の値を配列に設定し出力するコードを記述せよ。
 1 2  4   8  16
 1 3  9  27  81
 1 4 16  64 256
 1 5 25 125 625
-
 4.以下のフィボナッチ数列(20個)の出力となるコードを記述せよ。
-
 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
-
 5.以下の為替サイトよりBid(売値)、Ask(買値)、Change(始値比)を取得し出力せよ
 http://info.finance.yahoo.co.jp/fx/detail/?code=USDJPY=FX
 =end
@@ -46,93 +36,97 @@ require 'open-uri'
 require 'nokogiri'
 require 'openssl'
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
 puts '問1.1)'
 
 a = 1
-5.times do
+for i in 1..5 do
 	a.times do
 		print "*"
 	end
 	print "\n"
-	a = a + 1
+	a += 1
 end
 
-puts '問1.2-1)'
+puts '問1.2-1'
 
-a = 1
-b = 4
-5.times do
-	b.times do
+a = 4
+b = 1
+for i in 1..5 do
+	a.times do
 		print " "
 	end
-	a.times do
+	b.times do
 		print "*"
 	end
 	print "\n"
-	a = a + 2
-	b = b - 1
+	a -= 1
+	b += 2
 end
 
-puts '問1.2-2)'
+puts '問1.2-2'
 
-a = 9
-b = 0
-5.times do
-	b.times do
+a = 0
+b = 9
+
+for i in 1..5 do
+	a.times do
 		print " "
 	end
-	a.times do
+	b.times do
 		print "*"
 	end
 	print "\n"
-	a = a - 2
-	b = b + 1
+	b -= 2
+	a += 1
 end
 
 puts '問2'
 
-ans = 50
+num = 50
 i = 1
-puts ans
+puts num
 begin
-	ans = ans - i
-	puts ans
+	num = num - i
+	puts num
 	i += 1
-end while ans > 0
+end while num > 0
+print "\n"
 
 puts '問3'
 
-ary1 = []
-ary2 = []
-ary3 = []
-ary4 = []
-tmp = 1
+array1 = []
+array2 = []
+array3 = []
+array4 = []
 
-5.times do
-	ary1.push(tmp)
+tmp = 1
+for i in 1..5 do
+	array1.push(tmp)
 	tmp = tmp * 2
 end
+
 tmp = 1
-5.times do
-	ary2.push(tmp)
+for j in 1..5 do
+	array2.push(tmp)
 	tmp = tmp * 3
 end
+
 tmp = 1
-5.times do
-	ary3.push(tmp)
+for k in 1..5 do
+	array3.push(tmp)
 	tmp = tmp * 4
 end
+
 tmp = 1
-5.times do
-	ary4.push(tmp)
+for l in 1..5 do
+	array4.push(tmp)
 	tmp = tmp * 5
 end
-p ary1
-p ary2
-p ary3
-p ary4
+p array1
+p array2
+p array3
+p array4
+puts "\n"
 
 puts '問4'
 
@@ -145,6 +139,7 @@ end
 for i in 1...20
   print "#{num(i)} "
 end
+puts "\n"
 
 puts '問5'
 
